29431646e6424ee72a21ab8e663477a78bb8705c
Parvez Shaikh <parvezshaikhj@gmail.com>
2021-02-09 20:36:35 +0530

    Number Format Helper function updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import numberFormat from './utilities/numberFormat'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {numberFormat(50000)}
+      {numberFormat(150000)}
     </div>
   );
 }
---
+++ src/utilities/numberFormat.js
@@ -0,0 +1,6 @@
+const numberFormat = (value) => new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+}).format(value)
+
+export default numberFormat
