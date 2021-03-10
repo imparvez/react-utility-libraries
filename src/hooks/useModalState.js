2aa582d825fe47bf21b5ad4e364887998d32d5f5
Parvez Shaikh <parvezshaikhj@gmail.com>
2021-03-10 18:30:22 +0530

    Added useModalState hooks to update modal state to make in use in upcoming modal component

---
+++ src/hooks/useModalState.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const useModalState = (initialOpen = false) => {
+    const [isOpen, setIsOpen] = useState(initialOpen)
+
+    const onOpen = () => { setIsOpen(true) }
+
+    const onClose = () => { setIsOpen(false) }
+
+    const onToggle = () => { setIsOpen(!isOpen) }
+
+    return { onOpen, onClose, onToggle, isOpen }
+}
+
+export default useModalState
