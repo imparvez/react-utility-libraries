af5af8f336526be05736834c696a109acee28280
Parvez Shaikh <parvezshaikhj@gmail.com>
2021-03-10 18:31:21 +0530

    Added ExchangeCrypto component, numberFormat utility library, and useModalState hooks to initiate modal component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import numberFormat from './utilities/numberFormat'
+import ExchangeCrypto from './components/ExchangeCrypto/index'
+// Using Modal Hook
+import useModalState from './hooks/useModalState'
 
 function App() {
+  const { onToggle, isOpen} = useModalState()
+
+  const handleModalClick = () => {
+    onToggle();
+  }
+
   return (
     <div className="App">
+      {/* Testing of Number Format Intl Object */}
       {numberFormat(50000)}
       {numberFormat(150000)}
+      {/* Testing of Exchange Crypto */}
+      <ExchangeCrypto />
+      {/* Testing Modal Hook */}
+      <button onClick={() => handleModalClick()}>Open Modal</button>
+      <div>Modal State: {isOpen ? 'open' : 'close'}</div>
     </div>
   );
 }
